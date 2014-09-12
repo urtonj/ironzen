a9be9ed9472d3188195da0516724c667d533e439
Jason Urton <jason.urton@gmail.com>
2014-09-12 16:19:09 -0400

    Fix time range

--- app/controllers/workout_periods_controller.rb
+++ app/controllers/workout_periods_controller.rb
@@ -4,7 +4,7 @@ class WorkoutPeriodsController < ApplicationController
   def show
     current_user = User.find(ROOT_USER_ID)
 
-    workout_period = WorkoutPeriod.new(current_user, 3.months)
+    workout_period = WorkoutPeriod.new(current_user, 3.months.ago)
     @workout_graph_data = workout_period.graph_data
   end
 end
